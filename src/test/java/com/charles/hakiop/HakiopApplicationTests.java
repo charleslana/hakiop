74015224649a429bc37c2863002ddc9e427bee06
Charles <charles.lana@expertcode.com.br>
2022-09-14 10:09:15 -0300

    first commit

---
+++ src/test/java/com/charles/hakiop/HakiopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charles.hakiop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HakiopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
